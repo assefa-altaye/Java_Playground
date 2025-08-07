87f5717a1843c555b1a052d1664d253102d6822f
Assefa_Altaye <assekopsynwa@gmail.com>
2025-08-06 22:53:23 -0500

    Read From keyboard

---
+++ Java/ReadKeyBoard.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+public class ReadKeyBoard {
+    public static void main(String arg[]){
+        Scanner sc=new Scanner(System.in);
+        int x,y;
+        System.out.println("Enter two numbers");
+        x=sc.nextInt();
+        y=sc.nextInt();
+        int z= x+y;
+        System.out.println("Sum of two numbers is: " + z);
+    }
+}
