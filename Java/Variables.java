7f85803a93c4a3fdd0c0dc133290f331ade6ea0c
Assefa_Altaye <assekopsynwa@gmail.com>
2025-08-09 00:10:46 -0500

    Variables

---
+++ Java/Variables.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+
+class Variables{
+	public static void main(String[] args){
+	
+	byte b=5;
+	short s=400;
+	int i=4765;
+	float f=786.7f;
+	char c='A';
+
+	System.out.println("byte: " + b);
+	System.out.println("short: " + s);
+	System.out.println("int: " + i);
+	System.out.println("float: " + f);
+	System.out.println("char: " + c);
+	//error because Max_Value for byte is 127, 153 is out of range 
+	//b=153;
+	//System.out.println(b);
+	}
+}
