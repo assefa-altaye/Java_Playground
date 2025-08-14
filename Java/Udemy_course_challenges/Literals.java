7fd5f3d8d64944d4495f4140358f905ab0e4c42c
Assefa_Altaye <assekopsynwa@gmail.com>
2025-08-13 23:07:21 -0500

    variables

---
+++ Java/Udemy_course_challenges/Literals.java
@@ -0,0 +1,14 @@
+// package Udemy_course_challenges;
+// import java.lang.*;
+class Literals {
+    public static void main(String args[]){
+        byte b1=10; //decimal
+        byte b2=0b1010; //10 in binary
+        byte b3=012; //10 in octal
+        byte b4=0XA; //10 in hexadecimal
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+    }
+}
--- Java/Udemy_course_challenges/Variables.java
+++ Java/Udemy_course_challenges/Variables.java
@@ -1,22 +1,18 @@
-package Udemy_course_challenges;
+
 import java.lang.*;
-
-class Variables{
-	public static void main(String[] args){
-	
-	byte b=5;
-	short s=400;
-	int i=4765;
-	float f=786.7f;
-	char c='A';
-
-	System.out.println("byte: " + b);
-	System.out.println("short: " + s);
-	System.out.println("int: " + i);
-	System.out.println("float: " + f);
-	System.out.println("char: " + c);
-	//error because Max_Value for byte is 127, 153 is out of range 
-	//b=153;
-	//System.out.println(b);
-	}
+class Variables
+{
+    public static void main(String[] arg) 
+    {
+        byte b=127;
+        short s=300;
+        int i=100;
+        float f=23.45f;
+        char c='A';
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(c);
+    }
 }
